906a0ff67d4d3a0dc99a548ae88e0221c8d0ff76
algernon-A <aeneas@aloku.net>
2021-12-15 13:36:21 +1100

    Version bump to 1.1.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -11,7 +11,7 @@ namespace CurbHeightAdjuster
     public class CHAMod : IUserMod
     {
         public static string ModName => "Curb Height Adjuster";
-        public static string Version => "1.0.2";
+        public static string Version => "1.1";
 
         public string Name => ModName + " " + Version;
         public string Description => Translations.Translate("CHA_DESC");
